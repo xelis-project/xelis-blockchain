7085fe5dc60e7abeb0f86cd37c3e3eb8bb6714e9
Slixe <slixeprivate@gmail.com>
2023-01-10 16:16:10 +0100

    wallet: create encrypted storage system based on XChaCha20Poly1305

--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -2,15 +2,33 @@ pub mod transaction_builder;
 pub mod storage;
 
 use crate::config::{DEFAULT_DAEMON_ADDRESS, DEFAULT_DIR_PATH};
+use crate::core::error::BlockchainError;
 use crate::core::json_rpc::JsonRPCClient;
 use crate::crypto::key::KeyPair;
-
 use self::storage::Storage;
+use chacha20poly1305::aead::OsRng;
+use rand::RngCore;
+use thiserror::Error;
+use chacha20poly1305::Error as CryptoError;
 
+#[derive(Error, Debug)]
 pub enum WalletError {
+    #[error("Invalid key pair")]
     InvalidKeyPair,
+    #[error("Expected a TX")]
     ExpectedOneTx,
-    TxOwnerIsReceiver
+    #[error("Transaction owner is the receiver")]
+    TxOwnerIsReceiver,
+    #[error(transparent)]
+    CoreError(#[from] BlockchainError),
+    #[error("Error from crypto: {}", _0)]
+    CryptoError(CryptoError),
+    #[error("Unexpected error on database: {}", _0)]
+    DatabaseError(#[from] sled::Error),
+    #[error("Invalid encrypted value: minimum 25 bytes")]
+    InvalidEncryptedValue,
+    #[error("No salt found in storage")]
+    NoSalt
 }
 
 #[derive(Debug, clap::StructOpt)]
@@ -31,9 +49,11 @@ pub struct Wallet {
 
 impl Wallet {
     pub fn new(config: Config) -> Self {
+        let mut key: [u8; 32] = [0; 32]; 
+        OsRng.fill_bytes(&mut key);
         Wallet {
             keypair: KeyPair::new(),
-            storage: Storage::new(config.name).unwrap(),
+            storage: Storage::new(config.name, &key).unwrap(),
             client: JsonRPCClient::new(config.daemon_address)
         }
     }
