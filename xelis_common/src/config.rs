d8f8ce3f1fee5e6dfb7773376682b64283317380
Slixe <slixeprivate@gmail.com>
2023-10-17 14:11:58 +0200

    fix comments

--- xelis_common/src/config.rs
+++ xelis_common/src/config.rs
@@ -2,7 +2,7 @@ use crate::crypto::hash::Hash;
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const XELIS_ASSET: Hash = Hash::zero();
-// 0.01000 XLS per KB
+// 0.01000 XEL per KB
 pub const FEE_PER_KB: u64 = 1000;
 pub const COIN_DECIMALS: u8 = 5; // 5 decimals numbers
 pub const COIN_VALUE: u64 = 10u64.pow(COIN_DECIMALS as u32); // 100 000
--- xelis_miner/src/config.rs
+++ xelis_miner/src/config.rs
@@ -1,1 +1,2 @@
+// daemon address by default when no specified
 pub const DEFAULT_DAEMON_ADDRESS: &str = "127.0.0.1:8080";
