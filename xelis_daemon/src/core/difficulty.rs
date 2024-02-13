9a036df6872dfbdf8ef970ce7a32ef662a09decd
Slixe <slixeprivate@gmail.com>
2024-02-13 15:56:59 +0100

    daemon: delete previous difficulty adjustment algorithm

--- xelis_daemon/src/core/difficulty.rs
+++ xelis_daemon/src/core/difficulty.rs
@@ -1,46 +1,12 @@
-use std::f64::consts::E;
-
 use log::trace;
 use xelis_common::difficulty::Difficulty;
-use crate::config::{STABLE_LIMIT, BLOCK_TIME_MILLIS, MINIMUM_DIFFICULTY};
-
-const M: f64 = STABLE_LIMIT as f64;
-const BLOCK_TIME: f64 = BLOCK_TIME_MILLIS as f64;
-const FACTOR: i64 = 10000;
-
-// Calculate the difficulty for the next block
-// Difficulty jump can happen easily but drop is limited to 2x the block time
-// This is to prevent any attack on the difficulty where a miner would try to manipulate the network
-pub fn calculate_difficulty_v1(parent_timestamp: u128, new_timestamp: u128, previous_difficulty: Difficulty) -> Difficulty {
-    let mut solve_time = (new_timestamp - parent_timestamp) as f64;
-
-    // Limit to 2x the block time to prevent any too-big difficulty drop
-    if solve_time > BLOCK_TIME * 2f64 {
-        solve_time = BLOCK_TIME * 2f64;
-    }
-
-    let previous_difficulty: u64 = previous_difficulty.into();
-    let adjustment_factor = (E.powf((1f64 - solve_time as f64 / BLOCK_TIME) / M) * FACTOR as f64) as i64;
-    let diff = (((previous_difficulty as i64 * adjustment_factor) / FACTOR) as u64).into();
-    trace!("adjustment factor: {}, previous difficulty: {}, new difficulty: {}", adjustment_factor, previous_difficulty, diff);
-
-    if diff < MINIMUM_DIFFICULTY {
-       return MINIMUM_DIFFICULTY
-    }
-
-    diff
-}
+use crate::config::{BLOCK_TIME_MILLIS, MINIMUM_DIFFICULTY};
 
 const DIFFICULTY_BOUND_DIVISOR: Difficulty = Difficulty::from_u64(2048);
 const CHAIN_TIME_RANGE: u64 = BLOCK_TIME_MILLIS * 2 / 3;
 
 // Difficulty algorithm from Ethereum: Homestead but tweaked for our needs
 pub fn calculate_difficulty(tips_count: u64, parent_timestamp: u128, new_timestamp: u128, previous_difficulty: Difficulty) -> Difficulty {
-    // For current testnet, keep using same algorithm
-    if true {
-        return calculate_difficulty_v1(parent_timestamp, new_timestamp, previous_difficulty);
-    }
-
     let mut adjust = previous_difficulty / DIFFICULTY_BOUND_DIVISOR;
     let mut x = (new_timestamp - parent_timestamp) as u64 / CHAIN_TIME_RANGE;
     trace!("x: {x}, tips count: {tips_count}, adjust: {adjust}");
