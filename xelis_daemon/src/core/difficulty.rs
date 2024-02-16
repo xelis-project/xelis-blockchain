79dae2af61444655c2fffa8a1bad7a8b6b4dd2cb
Slixe <slixeprivate@gmail.com>
2024-02-16 17:01:23 +0100

    daemon: wip difficulty

--- xelis_daemon/src/core/difficulty.rs
+++ xelis_daemon/src/core/difficulty.rs
@@ -1,18 +1,24 @@
 use log::trace;
 use xelis_common::{
     difficulty::Difficulty,
-    time::TimestampMillis
+    time::TimestampMillis,
+    utils::format_difficulty
 };
 use crate::config::{BLOCK_TIME_MILLIS, MINIMUM_DIFFICULTY};
 
-const DIFFICULTY_BOUND_DIVISOR: Difficulty = Difficulty::from_u64(2048);
-const CHAIN_TIME_RANGE: TimestampMillis = BLOCK_TIME_MILLIS * 2 / 3;
+const DIFFICULTY_BOUND_DIVISOR: Difficulty = Difficulty::from_u64(100);
+const CHAIN_TIME_RANGE: TimestampMillis = BLOCK_TIME_MILLIS / 2;
 
 // Difficulty algorithm from Ethereum: Homestead but tweaked for our needs
 pub fn calculate_difficulty(tips_count: u64, parent_timestamp: TimestampMillis, new_timestamp: TimestampMillis, previous_difficulty: Difficulty) -> Difficulty {
     let mut adjust = previous_difficulty / DIFFICULTY_BOUND_DIVISOR;
-    let mut x = (new_timestamp - parent_timestamp) / CHAIN_TIME_RANGE;
-    trace!("x: {x}, tips count: {tips_count}, adjust: {adjust}");
+    let solve_time = new_timestamp - parent_timestamp;
+    // By how much we need to adjust the difficulty
+    // If we have a solve time of 30s and the range is 15s, we need to adjust by 2
+    // With a bound divisor of 100, we will adjust by 2% of the previous difficulty
+    let mut x = solve_time / CHAIN_TIME_RANGE;
+    trace!("x: {x}, tips count: {tips_count}, adjust: {adjust}, difficulty: {}, adjust: {}", format_difficulty(previous_difficulty), format_difficulty(adjust));
+
     let neg = x >= tips_count;
     if neg {
         x = x - tips_count;
@@ -23,6 +29,12 @@ pub fn calculate_difficulty(tips_count: u64, parent_timestamp: TimestampMillis, 
     // max(x, 99)
     if x > 99 {
         x = 99;
+    }
+
+    trace!("final x: {}", x);
+    // Returns the previous difficulty if no adjustment is needed
+    if x == 0 {
+        return previous_difficulty
     }
 
     let x: Difficulty = x.into();
