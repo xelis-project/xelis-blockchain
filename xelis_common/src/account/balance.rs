04794195c1bb963119a9ddbb4ad411b6eb57bf85
Slixe <slixeprivate@gmail.com>
2024-02-25 16:04:42 +0100

    common: output & final balance

--- xelis_common/src/account/balance.rs
+++ xelis_common/src/account/balance.rs
@@ -15,14 +15,21 @@ pub type BalanceRepresentation = CompressedCiphertext;
 
 #[derive(Clone, Deserialize, Serialize)]
 pub struct VersionedBalance {
-    balance: BalanceRepresentation,
+    // Output balance is used in case of multi TXs not in same block
+    // If you build several TXs at same time but are not in the same block,
+    // and a incoming tx happen we need to keep track of the output balance
+    output_balance: Option<BalanceRepresentation>,
+    // Final user balance that contains outputs and inputs balance
+    // This is the balance shown to a user and used to build TXs
+    final_balance: BalanceRepresentation,
     previous_topoheight: Option<u64>,
 }
 
 impl VersionedBalance {
     pub const fn new(balance: BalanceRepresentation, previous_topoheight: Option<u64>) -> Self {
         Self {
-            balance,
+            output_balance: None,
+            final_balance: balance,
             previous_topoheight
         }
     }
@@ -34,25 +41,34 @@ impl VersionedBalance {
         ).compress();
 
         Self {
-            balance: zero,
+            output_balance: None,
+            final_balance: zero,
             previous_topoheight: None
         }
     }
 
     pub fn get_balance(&self) -> &BalanceRepresentation {
-        &self.balance
+        &self.final_balance
     }
 
     pub fn get_mut_balance(&mut self) -> &mut BalanceRepresentation {
-        &mut self.balance
+        &mut self.final_balance
     }
 
     pub fn take_balance(self) -> BalanceRepresentation {
-        self.balance
+        self.final_balance
+    }
+
+    pub fn set_output_balance(&mut self, value: BalanceRepresentation) {
+        self.output_balance = Some(value);
+    }
+
+    pub fn get_output_balance(&self) -> Option<&BalanceRepresentation> {
+        self.output_balance.as_ref()
     }
 
     pub fn set_balance(&mut self, value: BalanceRepresentation) {
-        self.balance = value;
+        self.final_balance = value;
     }
 
     pub fn add_plaintext_to_balance(&mut self, _value: u64) {
@@ -76,33 +92,43 @@ impl Default for VersionedBalance {
 
 impl Display for VersionedBalance {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Balance[ciphertext[{}, {}], previous: {:?}", hex::encode(self.balance.0[0]), hex::encode(self.balance.0[1]), self.previous_topoheight)
+        write!(f, "Balance[ciphertext[{}, {}], previous: {:?}", hex::encode(self.final_balance.0[0]), hex::encode(self.final_balance.0[1]), self.previous_topoheight)
     }
 }
 
 impl Serializer for VersionedBalance {
     fn write(&self, writer: &mut Writer) {
-        self.balance.write(writer);
+        self.final_balance.write(writer);
         if let Some(topo) = &self.previous_topoheight {
             writer.write_u64(topo);
+        }
+        if let Some(output) = &self.output_balance {
+            output.write(writer);
         }
     }
 
     fn read(reader: &mut Reader) -> Result<Self, ReaderError> {
-        let balance = BalanceRepresentation::read(reader)?;
-        let previous_topoheight = if reader.size() == 0 {
-            None
+        let final_balance = BalanceRepresentation::read(reader)?;
+        let (previous_topoheight, output_balance) = if reader.size() == 0 {
+            (None, None)
         } else {
-            Some(reader.read_u64()?)
+            if reader.size() == 8 {
+                (Some(reader.read_u64()?), None)
+            } else {
+                (Some(reader.read_u64()?), Some(BalanceRepresentation::read(reader)?))
+            }
         };
 
         Ok(Self {
-            balance,
+            output_balance,
+            final_balance,
             previous_topoheight
         })
     }
 
     fn size(&self) -> usize {
-        self.balance.size() + if let Some(topoheight) = self.previous_topoheight { topoheight.size() } else { 0 }
+        self.final_balance.size()
+        + if let Some(topoheight) = self.previous_topoheight { topoheight.size() } else { 0 }
+        + if let Some(output_balance) = self.output_balance { output_balance.size() } else { 0 }
     }
 }
