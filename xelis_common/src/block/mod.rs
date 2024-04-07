339ce65537eb44822f41f81e2193d791cd9f44a8
Slixe <slixeprivate@gmail.com>
2024-04-07 23:37:28 +0200

    common: tips_merkle_hash in block header

--- xelis_common/src/block/mod.rs
+++ xelis_common/src/block/mod.rs
@@ -6,6 +6,20 @@ pub use header::BlockHeader;
 pub use block::Block;
 pub use miner::BlockMiner;
 
+use crate::crypto::{Hash, HASH_SIZE};
+
 pub const EXTRA_NONCE_SIZE: usize = 32;
-pub const HEADER_WORK_SIZE: usize = 105;
+pub const HEADER_WORK_SIZE: usize = 137;
 pub const BLOCK_WORK_SIZE: usize = 112; // 32 + 8 + 8 + 32 + 32 = 112
+
+// Get combined hash for tips
+// This is used to get a hash that is unique for a set of tips
+pub fn get_combined_hash_for_tips<'a, I: Iterator<Item = &'a Hash>>(tips: I) -> Hash {
+    let mut bytes = [0u8; HASH_SIZE];
+    for tip in tips {
+        for i in 0..HASH_SIZE {
+            bytes[i] ^= tip.as_bytes()[i];
+        }
+    }
+    Hash::new(bytes)
+}
