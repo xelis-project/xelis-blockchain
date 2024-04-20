f119da90289247c430bf2f5e1f4aef9dd4afb812
Slixe <slixeprivate@gmail.com>
2024-04-20 15:06:11 +0200

    wallet: fix seed check

--- xelis_wallet/src/main.rs
+++ xelis_wallet/src/main.rs
@@ -545,7 +545,7 @@ async fn recover_wallet(manager: &CommandManager, _: ArgumentManager) -> Result<
         .await.context("Error while reading seed")?;
 
     let words_count = seed.split_whitespace().count();
-    if words_count != 25 || words_count != 24 {
+    if words_count != 25 && words_count != 24 {
         manager.error("Seed must be 24 or 25 (checksum) words long");
         return Ok(())
     }
