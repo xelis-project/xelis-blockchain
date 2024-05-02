7bc8289411ad13a771626024feb047828f9c4342
Slixe <slixeprivate@gmail.com>
2024-05-02 11:51:18 +0200

    common: fix subscribe response

--- xelis_common/src/rpc_server/websocket/handler.rs
+++ xelis_common/src/rpc_server/websocket/handler.rs
@@ -102,12 +102,12 @@ where
             "subscribe" => {
                 let event = self.parse_event(&mut request)?;
                 self.subscribe_session_to_event(context.get::<WebSocketSessionShared<Self>>().unwrap(), event, request.id.clone()).await?;
-                Ok(Some(Value::Bool(true)))
+                Ok(Some(json!(RpcResponse::new(Cow::Borrowed(&request.id), Cow::Owned(Value::Bool(true))))))
             },
             "unsubscribe" => {
                 let event = self.parse_event(&mut request)?;
                 self.unsubscribe_session_from_event(context.get::<WebSocketSessionShared<Self>>().unwrap(), event, request.id.clone()).await?;
-                Ok(Some(Value::Bool(true)))
+                Ok(Some(json!(RpcResponse::new(Cow::Borrowed(&request.id), Cow::Owned(Value::Bool(true))))))
             },
             _ => self.handler.execute_method(context, request).await
         }
