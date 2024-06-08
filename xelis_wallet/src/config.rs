e95488eb8da0122f88599f0da6e792645b45e033
0xshwifty <62264499+0xshwifty@users.noreply.github.com>
2024-06-08 22:12:48 +0500

    Update config.rs

--- xelis_wallet/src/config.rs
+++ xelis_wallet/src/config.rs
@@ -8,7 +8,7 @@ pub const SALT_SIZE: usize = 32;
 pub const KEY_SIZE: usize = 32;
 
 // daemon address by default when no specified
-pub const DEFAULT_DAEMON_ADDRESS: &str = "http://127.0.0.1:8080";
+pub const DEFAULT_DAEMON_ADDRESS: &str = "http://127.0.0.1:6666";
 // Auto reconnect interval in seconds for Network Handler
 pub const AUTO_RECONNECT_INTERVAL: u64 = 5;
 
