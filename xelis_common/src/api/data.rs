69946b4a20138a0c4f9cdff85284f79a6e7eb5f4
Slixe <slixeprivate@gmail.com>
2024-06-19 00:38:52 +0200

    common: remove panic, use TryInto,  move Blob to DataValue

--- xelis_common/src/api/data.rs
+++ xelis_common/src/api/data.rs
@@ -32,7 +32,8 @@ pub enum ValueType {
     U32,
     U64,
     U128,
-    Hash
+    Hash,
+    Blob,
 }
 
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Hash, Clone)]
@@ -43,8 +44,6 @@ pub enum ElementType {
     Array,
     // Map<K, V> of elements
     Fields,
-    // Binary data
-    Blob,
 }
 
 impl ValueType {
@@ -71,6 +70,7 @@ impl Serializer for ValueType {
             5 => Self::U64,
             6 => Self::U128,
             7 => Self::Hash,
+            8 => Self::Blob,
             _ => return Err(ReaderError::InvalidValue)
         })
     }
@@ -84,7 +84,8 @@ impl Serializer for ValueType {
             Self::U32 => 4,
             Self::U64 => 5,
             Self::U128 => 6,
-            Self::Hash => 7
+            Self::Hash => 7,
+            Self::Blob => 8
         });
     }
 
@@ -99,10 +100,6 @@ impl Serializer for ValueType {
 #[serde(untagged)]
 pub enum DataElement {
     Value(DataValue),
-    // This is a specific type for optimized size of binary data
-    // Because above variants rewrite for each element the byte of the element and of each value
-    // It supports up to 65535 bytes (u16::MAX)
-    Blob(Vec<u8>),
     // For two next variants, we support up to 255 (u8::MAX) elements maximum
     Array(Vec<DataElement>),
     // Key-Value map with key as DataValue and value as DataElement
@@ -145,7 +142,6 @@ impl DataElement {
             Self::Array(_) => ElementType::Array,
             Self::Fields(_) => ElementType::Fields,
             Self::Value(value) => ElementType::Value(value.kind()),
-            Self::Blob(_) => ElementType::Blob,
         }
     }
 
@@ -217,7 +213,6 @@ impl Serializer for DataElement {
                 }
                 Self::Fields(fields)
             },
-            3 => Self::Blob(Vec::read(reader)?),
             _ => return Err(ReaderError::InvalidValue)
         })
     }
@@ -243,10 +238,6 @@ impl Serializer for DataElement {
                     value.write(writer);
                 }
             }
-            Self::Blob(data) => {
-                writer.write_u8(3);
-                data.write(writer);
-            }
         }
     }
 
@@ -266,8 +257,7 @@ impl Serializer for DataElement {
                     size += key.size() + value.size();
                 }
                 size
-            },
-            Self::Blob(data) => 2 + data.len()
+            }
         }
     }
 }
@@ -283,6 +273,10 @@ pub enum DataValue {
     U64(u64),
     U128(u128),
     Hash(Hash),
+    // This is a specific type for optimized size of binary data
+    // Because above variants rewrite for each element the byte of the element and of each value
+    // It supports up to 65535 bytes (u16::MAX)
+    Blob(Vec<u8>),
 }
 
 impl DataValue {
@@ -295,7 +289,8 @@ impl DataValue {
             Self::U32(_) => ValueType::U32,
             Self::U64(_) => ValueType::U64,
             Self::U128(_) => ValueType::U128,
-            Self::Hash(_) => ValueType::Hash
+            Self::Hash(_) => ValueType::Hash,
+            Self::Blob(_) => ValueType::Blob
         }
     }
 
@@ -420,7 +415,8 @@ impl DataValue {
             ValueType::U32 => Self::U32(reader.read_u32()?),
             ValueType::U64 => Self::U64(reader.read_u64()?),
             ValueType::U128 => Self::U128(reader.read_u128()?),
-            ValueType::Hash => Self::Hash(reader.read_hash()?)
+            ValueType::Hash => Self::Hash(reader.read_hash()?),
+            ValueType::Blob => Self::Blob(Vec::read(reader)?)
         })
     }
 
@@ -449,6 +445,9 @@ impl DataValue {
             },
             Self::Hash(hash) => {
                 writer.write_hash(hash);
+            },
+            Self::Blob(blob) => {
+                blob.write(writer);
             }
         };
     }
@@ -464,7 +463,8 @@ impl ToString for DataValue {
             Self::U32(v) => format!("{}", v),
             Self::U64(v) => format!("{}", v),
             Self::U128(v) => format!("{}", v),
-            Self::Hash(v) => format!("{}", v)
+            Self::Hash(v) => format!("{}", v),
+            Self::Blob(v) => format!("{:?}", v)
         }
     }
 }
@@ -489,13 +489,37 @@ impl Serializer for DataValue {
             Self::U32(v) => v.size(),
             Self::U64(v) => v.size(),
             Self::U128(v) => v.size(),
-            Self::Hash(hash) => hash.size()
+            Self::Hash(hash) => hash.size(),
+            Self::Blob(blob) => blob.size()
         };
         // 1 byte for the type
         size + 1
     }
 }
 
+macro_rules! impl_data_value_vec {
+    ($(($type:ident, $type2:ident)),*) => {
+        $(
+            impl From<Vec<$type2>> for DataElement {
+                fn from(value: Vec<$type2>) -> Self {
+                    Self::Array(value.into_iter().map(|v| v.into()).collect())
+                }
+            }
+
+            impl TryInto<Vec<$type2>> for DataElement {
+                type Error = DataConversionError;
+
+                fn try_into(self) -> Result<Vec<$type2>, Self::Error> {
+                    match self {
+                        Self::Array(v) => v.into_iter().map(|v| v.try_into()).collect(),
+                        _ => Err(DataConversionError::ExpectedArray)
+                    }
+                }
+            }
+        )*
+    };
+
+}
 macro_rules! impl_data_value {
     ($(($type:ident, $type2:ident)),*) => {
         $(
@@ -511,14 +535,10 @@ macro_rules! impl_data_value {
                 }
             }
 
-            impl From<Vec<$type2>> for DataElement {
-                fn from(value: Vec<$type2>) -> Self {
-                    DataElement::Array(value.into_iter().map(|v| v.into()).collect())
-                }
-            }
+            impl TryInto<$type2> for DataValue {
+                type Error = DataConversionError;
 
-            impl Into<Result<$type2, DataConversionError>> for DataValue {
-                fn into(self) -> Result<$type2, DataConversionError> {
+                fn try_into(self) -> Result<$type2, Self::Error> {
                     match self {
                         Self::$type(v) => Ok(v),
                         _ => Err(DataConversionError::UnexpectedValue(self.kind()))
@@ -526,71 +546,21 @@ macro_rules! impl_data_value {
                 }
             }
 
-            impl Into<Result<$type2, DataConversionError>> for DataElement {
-                fn into(self) -> Result<$type2, DataConversionError> {
+            impl TryInto<$type2> for DataElement {
+                type Error = DataConversionError;
+
+                fn try_into(self) -> Result<$type2, Self::Error> {
                     match self {
-                        DataElement::Value(v) => v.into(),
+                        DataElement::Value(v) => v.try_into(),
                         _ => Err(DataConversionError::ExpectedValue)
-                    }
-                }
-            }
-
-            impl Into<Result<Vec<$type2>, DataConversionError>> for DataElement {
-                fn into(self) -> Result<Vec<$type2>, DataConversionError> {
-                    match self {
-                        DataElement::Array(v) => v.into_iter().map(|v| v.into()).collect::<Result<Vec<_>, DataConversionError>>(),
-                        _ => Err(DataConversionError::ExpectedValue)
-                    }
-                }
-            }
-
-            impl Into<Option<$type2>> for DataValue {
-                fn into(self) -> Option<$type2> {
-                    match self {
-                        Self::$type(v) => Some(v),
-                        _ => None
-                    }
-                }
-            }
-
-            impl Into<Option<$type2>> for DataElement {
-                fn into(self) -> Option<$type2> {
-                    match self {
-                        DataElement::Value(v) => v.into(),
-                        _ => None
-                    }
-                }
-            }
-
-            impl Into<$type2> for DataValue {
-                fn into(self) -> $type2 {
-                    match self {
-                        Self::$type(v) => v,
-                        _ => panic!("Unexpected value type")
-                    }
-                }
-            }
-
-            impl Into<$type2> for DataElement {
-                fn into(self) -> $type2 {
-                    match self {
-                        DataElement::Value(v) => v.into(),
-                        _ => panic!("Unexpected element type")
-                    }
-                }
-            }
-
-            impl Into<Vec<$type2>> for DataElement {
-                fn into(self) -> Vec<$type2> {
-                    match self {
-                        DataElement::Array(v) => v.into_iter().map(|v| v.into()).collect(),
-                        _ => panic!("Unexpected element type")
                     }
                 }
             }
         )*
     };
 }
+
+type Blob = Vec<u8>;
 
 impl_data_value!(
     (String, String),
@@ -600,7 +570,20 @@ impl_data_value!(
     (U32, u32),
     (U64, u64),
     (U128, u128),
-    (Bool, bool)
+    (Bool, bool),
+    (Blob, Blob)
+);
+
+// u8 is missing because it's already implemented by Blob type
+impl_data_value_vec!(
+    (String, String),
+    (Hash, Hash),
+    (U16, u16),
+    (U32, u32),
+    (U64, u64),
+    (U128, u128),
+    (Bool, bool),
+    (Blob, Blob)
 );
 
 #[cfg(test)]
@@ -612,11 +595,11 @@ mod tests {
         let value = DataValue::U8(10);
         let element = DataElement::Value(value.clone());
         assert_eq!(element.size(), element.to_bytes().len());
-        let value2: u8 = element.into();
+        let value2: u8 = element.try_into().unwrap();
         assert_eq!(value2, 10);
 
         let array: DataElement = vec![0u64, 24u64, 37u64, 55u64].into();
-        let array2: Vec<u64> = array.into();
+        let array2: Vec<u64> = array.try_into().unwrap();
         assert_eq!(array2, vec![0, 24, 37, 55]);
     }
 
@@ -628,7 +611,7 @@ mod tests {
         assert_eq!(data, elem);
         assert_eq!(data.size(), elem.to_bytes().len());
 
-        let val2: Vec<u64> = elem.into();
+        let val2: Vec<u64> = elem.try_into().unwrap();
         assert_eq!(val, val2);
     }
 
@@ -636,12 +619,12 @@ mod tests {
     fn test_blob() {
         let data = vec![0u8; 1000];
         let element: DataElement = data.clone().into();
-        let element2: Vec<u8> = element.into();
+        let element2: Vec<u8> = element.try_into().unwrap();
         assert_eq!(data, element2);
 
         let json = "[0, 55, 77, 99, 88, 77]";
         let element: DataElement = serde_json::from_str(json).unwrap();
-        assert_eq!(element.kind(), ElementType::Blob);
+        assert_eq!(element.kind(), ElementType::Value(ValueType::Blob));
     }
 
     #[test]
