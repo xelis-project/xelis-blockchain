7ade374047495d018d95cd9e96dcd4a48f52ad09
Slixe <slixeprivate@gmail.com>
2024-11-02 21:12:15 +0100

    common: bech32 pub consts

--- xelis_common/src/crypto/bech32.rs
+++ xelis_common/src/crypto/bech32.rs
@@ -2,9 +2,13 @@ use std::string::FromUtf8Error;
 
 use thiserror::Error;
 
-const CHARSET: &str = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
-const GENERATOR: [u32; 5] = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
-const SEPARATOR: char = ':';
+/// Bech32 constants.
+/// character set used in Bech32 encoding.
+pub const CHARSET: &str = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
+/// generator polynomial used in Bech32 encoding.
+pub const GENERATOR: [u32; 5] = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3];
+/// separator character used in Bech32 encoding.
+pub const SEPARATOR: char = ':';
 
 #[derive(Error, Debug)]
 pub enum Bech32Error {
