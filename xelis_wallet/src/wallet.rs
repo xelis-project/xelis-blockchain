d55c6e48d16e150593cd958921422a0f355619ce
Slixe <slixeprivate@gmail.com>
2024-11-28 16:07:03 +0100

    wallet: don't apply changes on create_transaction_with_storage

--- xelis_wallet/src/wallet.rs
+++ xelis_wallet/src/wallet.rs
@@ -627,19 +627,20 @@ impl Wallet {
     pub async fn create_transaction(&self, transaction_type: TransactionTypeBuilder, fee: FeeBuilder) -> Result<Transaction, WalletError> {
         trace!("create transaction");
         let mut storage = self.storage.write().await;
-        self.create_transaction_with_storage(&mut storage, transaction_type, fee).await
+        let (tx, mut state) = self.create_transaction_with_storage(&storage, transaction_type, fee).await?;
+        state.apply_changes(&mut storage).await?;
+
+        Ok(tx)
     }
 
     // Create a transaction with the given transaction type and fee
     // this will apply the changes to the storage if the transaction
-    pub async fn create_transaction_with_storage(&self, storage: &mut EncryptedStorage, transaction_type: TransactionTypeBuilder, fee: FeeBuilder) -> Result<Transaction, WalletError> {
+    pub async fn create_transaction_with_storage(&self, storage: &EncryptedStorage, transaction_type: TransactionTypeBuilder, fee: FeeBuilder) -> Result<(Transaction, TransactionBuilderState), WalletError> {
         trace!("create transaction with storage");
         let mut state = self.create_transaction_state_with_storage(&storage, &transaction_type, &fee, None).await?;
         let transaction = self.create_transaction_with(&mut state, storage.get_tx_version().await?, transaction_type, fee)?;
 
-        state.apply_changes(storage).await?;
-
-        Ok(transaction)
+        Ok((transaction, state))
     }
 
     // create the final transaction with calculated fees and signature
