27d35d3f6b4de56739c67d4147263be4ac9d2dec
Slixe <slixeprivate@gmail.com>
2024-12-11 10:24:44 +0100

    common: history synced event

--- xelis_common/src/api/wallet.rs
+++ xelis_common/src/api/wallet.rs
@@ -284,6 +284,9 @@ pub enum NotifyEvent {
     // When a rescan happened on the wallet
     // Contains a topoheight as value to indicate until which topoheight transactions got deleted
     Rescan,
+    // When the history has been synced again
+    // Contains current topoheight as value
+    HistorySynced,
     // When network state changed
     Online,
     // Same here
