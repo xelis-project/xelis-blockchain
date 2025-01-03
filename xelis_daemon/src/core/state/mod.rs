c37910de41555ada70b8d9c89d7e196b4431ab57
Slixe <slixeprivate@gmail.com>
2025-01-03 02:37:46 +0100

    daemon: debug output range

--- xelis_daemon/src/core/state/mod.rs
+++ xelis_daemon/src/core/state/mod.rs
@@ -107,6 +107,7 @@ pub (super) async fn search_versioned_balance_for_reference<S: DagOrderProvider 
         .min(reference_block_topo)
         .min(current_topoheight);
 
+    debug!("Search output balance in range {} to {}", min_topo, current_topoheight);
     let last_output = storage.get_output_balance_in_range(key, asset, min_topo, current_topoheight).await?;
     // We have a output balance
     if let Some((topo, v)) = last_output {
