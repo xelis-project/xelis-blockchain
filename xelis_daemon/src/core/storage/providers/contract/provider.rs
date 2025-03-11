cc14a8f518d32da9f69d6dedc4c4cb4fc7fc5002
Slixe <slixeprivate@gmail.com>
2025-03-11 10:16:22 +0100

    daemon: use try block on from tokio instead of futures executor

--- xelis_daemon/src/core/storage/providers/contract/provider.rs
+++ xelis_daemon/src/core/storage/providers/contract/provider.rs
@@ -3,7 +3,8 @@ use xelis_common::{
     asset::AssetData,
     block::TopoHeight,
     contract::{ContractProvider, ContractStorage},
-    crypto::{Hash, PublicKey}
+    crypto::{Hash, PublicKey},
+    tokio::try_block_on
 };
 use xelis_vm::Constant;
 use crate::core::storage::{
@@ -19,7 +20,7 @@ use crate::core::storage::{
 impl ContractStorage for SledStorage {
     fn load_data(&self, contract: &Hash, key: &Constant, topoheight: TopoHeight) -> Result<Option<(TopoHeight, Option<Constant>)>, anyhow::Error> {
         trace!("load contract {} key {} data at topoheight {}", contract, key, topoheight);
-        let res = futures::executor::block_on(self.get_contract_data_at_maximum_topoheight_for(contract, &key, topoheight))?;
+        let res = try_block_on(self.get_contract_data_at_maximum_topoheight_for(contract, &key, topoheight))??;
 
         match res {
             Some((topoheight, data)) => match data.take() {
@@ -32,13 +33,13 @@ impl ContractStorage for SledStorage {
 
     fn has_data(&self, contract: &Hash, key: &Constant, topoheight: TopoHeight) -> Result<bool, anyhow::Error> {
         trace!("check if contract {} key {} data exists at topoheight {}", contract, key, topoheight);
-        let contains = futures::executor::block_on(self.has_contract_data_at_topoheight(contract, &key, topoheight))?;
+        let contains = try_block_on(self.has_contract_data_at_topoheight(contract, &key, topoheight))??;
         Ok(contains)
     }
 
     fn load_data_latest_topoheight(&self, contract: &Hash, key: &Constant, topoheight: TopoHeight) -> Result<Option<TopoHeight>, anyhow::Error> {
         trace!("load data latest topoheight for contract {} key {} at topoheight {}", contract, key, topoheight);
-        let res = futures::executor::block_on(self.get_contract_data_topoheight_at_maximum_topoheight_for(contract, &key, topoheight))?;
+        let res = try_block_on(self.get_contract_data_topoheight_at_maximum_topoheight_for(contract, &key, topoheight))??;
         Ok(res)
     }
 }
@@ -46,20 +47,20 @@ impl ContractStorage for SledStorage {
 impl ContractProvider for SledStorage {
     fn get_contract_balance_for_asset(&self, contract: &Hash, asset: &Hash, topoheight: TopoHeight) -> Result<Option<(TopoHeight, u64)>, anyhow::Error> {
         trace!("get contract balance for contract {} asset {}", contract, asset);
-        let res =futures::executor::block_on(self.get_contract_balance_at_maximum_topoheight(contract, asset, topoheight))?;
+        let res = try_block_on(self.get_contract_balance_at_maximum_topoheight(contract, asset, topoheight))??;
         Ok(res.map(|(topoheight, balance)| (topoheight, balance.take())))
     }
 
     fn asset_exists(&self, asset: &Hash, topoheight: TopoHeight) -> Result<bool, anyhow::Error> {
         trace!("check if asset {} exists at topoheight {}", asset, topoheight);
-        let contains = futures::executor::block_on(self.has_asset_at_topoheight(asset, topoheight))?;
+        let contains = try_block_on(self.has_asset_at_topoheight(asset, topoheight))??;
         Ok(contains)
     }
 
     fn account_exists(&self, key: &PublicKey, topoheight: TopoHeight) -> Result<bool, anyhow::Error> {
         trace!("check if account {} exists at topoheight {}", key.as_address(self.is_mainnet()), topoheight);
 
-        let contains = futures::executor::block_on(self.is_account_registered_for_topoheight(key, topoheight))?;
+        let contains = try_block_on(self.is_account_registered_for_topoheight(key, topoheight))??;
         Ok(contains)
     }
 
