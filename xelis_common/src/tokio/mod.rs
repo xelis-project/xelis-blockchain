c346691c57a7bfc27b5e56c20fa9d906753f818c
Slixe <slixeprivate@gmail.com>
2025-03-13 22:21:37 +0100

    common: tx verify support without tokio feature enabled, fallback on futures if single thread block on

--- xelis_common/src/tokio/mod.rs
+++ xelis_common/src/tokio/mod.rs
@@ -1,34 +1,45 @@
+#[cfg(feature = "tokio")]
 mod thread_pool;
 
 use std::future::Future;
 use log::trace;
 
+
+#[cfg(feature = "tokio")]
 pub use thread_pool::ThreadPool;
 
 #[cfg(all(
+    feature = "tokio",
     target_arch = "wasm32",
     target_vendor = "unknown",
     target_os = "unknown"
 ))]
 pub use tokio_with_wasm::*;
 
-#[cfg(not(all(
-    target_arch = "wasm32",
-    target_vendor = "unknown",
-    target_os = "unknown"
-)))]
+#[cfg(all(
+    feature = "tokio",
+    not(all(
+        target_arch = "wasm32",
+        target_vendor = "unknown",
+        target_os = "unknown"
+    ))
+))]
 pub use tokio::*;
 
+#[cfg(feature = "tokio")]
 use runtime::{Handle, RuntimeFlavor};
 
 // Spawn a new task with a name
 // If the tokio_unstable feature is enabled, the task will be named
 #[inline(always)]
-#[cfg(not(all(
-    target_arch = "wasm32",
-    target_vendor = "unknown",
-    target_os = "unknown"
-)))]
+#[cfg(all(
+    feature = "tokio",
+    not(all(
+        target_arch = "wasm32",
+        target_vendor = "unknown",
+        target_os = "unknown"
+    ))
+))]
 #[track_caller]
 pub fn spawn_task<F, S: Into<String>>(name: S, future: F) -> task::JoinHandle<F::Output>
 where
@@ -51,6 +62,7 @@ where
 // Spawn a new task with a name
 // Send trait is not required for wasm32
 #[cfg(all(
+    feature = "tokio",
     target_arch = "wasm32",
     target_vendor = "unknown",
     target_os = "unknown"
@@ -68,20 +80,37 @@ where
 // Verify if the multi thread is supported by the caller
 pub fn is_multi_threads_supported() -> bool {
     trace!("is multi thread supported");
-    let supported = Handle::try_current()
-        .map(|v| matches!(v.runtime_flavor(), RuntimeFlavor::MultiThread))
-        .unwrap_or(false);
-    trace!("multi threads supported: {}", supported);
-
-    supported
+    #[cfg(feature = "tokio")]
+    {
+        let supported = Handle::try_current()
+            .map(|v| matches!(v.runtime_flavor(), RuntimeFlavor::MultiThread))
+            .unwrap_or(false);
+        trace!("multi threads supported: {}", supported);
+    
+        supported
+    }
+    #[cfg(not(feature = "tokio"))]
+    false
 }
 
 // Try blocking on using the current executor available for the thread
 // If the current executor is not available, return an error
 pub fn try_block_on<F: Future>(future: F) -> Result<F::Output, anyhow::Error> {
     trace!("try block on");
-    let handle = Handle::try_current()?;
-    Ok(handle.block_on(future))
+    #[cfg(feature = "tokio")]
+    {
+        let handle = Handle::try_current()?;
+        if matches!(handle.runtime_flavor(), RuntimeFlavor::CurrentThread) {
+            trace!("runtime is current thread and may not support blocking, fallback on futures executor");
+            Ok(futures::executor::block_on(future))
+        } else {
+            Ok(handle.block_on(future))
+        }
+    }
+    #[cfg(not(feature = "tokio"))]
+    {
+        Err(anyhow::anyhow!("Tokio feature is not enabled"))
+    }
 }
 
 // Block in place if multi thread is supported
