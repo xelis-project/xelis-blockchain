17165938c51e45b9fe985baef5cb648aeebe634e
Slixe <slixeprivate@gmail.com>
2025-05-24 21:17:08 +0200

    common: hash/eq custom impl for Immutable

--- xelis_common/src/immutable.rs
+++ xelis_common/src/immutable.rs
@@ -1,13 +1,11 @@
 use std::{
-    fmt::{self, Display},
-    ops::Deref,
-    sync::Arc
+    fmt::{self, Display}, hash::{Hash, Hasher}, ops::Deref, sync::Arc
 };
 use serde::{Serialize, Deserialize};
 
 use crate::serializer::*;
 
-#[derive(Clone, Serialize, Deserialize, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Serialize, Deserialize, Debug)]
 #[serde(untagged)]
 pub enum Immutable<T> {
     Owned(T),
@@ -65,6 +63,21 @@ impl<T: Clone> Immutable<T> {
     }
 }
 
+impl<T: Hash> Hash for Immutable<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.as_ref().hash(state);
+    }
+}
+
+impl<T: PartialEq> PartialEq for Immutable<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.as_ref().eq(other.as_ref())
+    }
+}
+
+impl<T: Eq> Eq for Immutable<T> {}
+
+
 impl<T> AsRef<T> for Immutable<T> {
     fn as_ref(&self) -> &T {
         self.get_inner()
@@ -111,5 +124,33 @@ impl<T: Serializer> Serializer for Immutable<T> {
 
     fn size(&self) -> usize {
         self.as_ref().size()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::hash::{DefaultHasher, Hash, Hasher};
+
+    use super::*;
+
+    #[test]
+    fn test_eq() {
+        assert!(Immutable::Owned(()) == Immutable::Arc(Arc::new(())));
+    }
+
+    #[test]
+    fn test_hash() {
+        let owned = Immutable::Owned(());
+        let arc = Immutable::Arc(Arc::new(()));
+
+        let mut hasher = DefaultHasher::new();
+        owned.hash(&mut hasher);
+        let owned_hash = hasher.finish();
+
+        let mut hasher = DefaultHasher::new();
+        arc.hash(&mut hasher);
+        let arc_hash = hasher.finish();
+
+        assert_eq!(owned_hash, arc_hash);
     }
 }
