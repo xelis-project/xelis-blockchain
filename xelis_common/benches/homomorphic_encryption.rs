8c9f3949d2bd8134094a2590bfccec8b4644f50c
Slixe <slixeprivate@gmail.com>
2025-06-19 10:46:53 +0200

    common: use black_box std

--- xelis_common/benches/homomorphic_encryption.rs
+++ xelis_common/benches/homomorphic_encryption.rs
@@ -1,4 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 use curve25519_dalek::Scalar;
 use xelis_common::crypto::KeyPair;
 
