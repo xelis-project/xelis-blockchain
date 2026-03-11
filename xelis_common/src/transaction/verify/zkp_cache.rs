aaa3256038ea492c311f8b2ce4c6de22765e19a0
Slixe <slixeprivate@gmail.com>
2026-03-11 14:31:23 +0100

    common: add trusted zkp cache

--- xelis_common/src/transaction/verify/zkp_cache.rs
+++ xelis_common/src/transaction/verify/zkp_cache.rs
@@ -8,6 +8,7 @@ pub trait ZKPCache<E> {
     async fn is_already_verified(&self, hash: &Hash) -> Result<bool, E>;
 }
 
+/// A ZKPCache that always returns false, for use in tests or when we are sure that the proofs are not verified yet
 #[derive(Default)]
 pub struct NoZKPCache;
 
@@ -16,5 +17,15 @@ impl<E> ZKPCache<E> for NoZKPCache {
     // Did we already verified the ZK Proofs of this transaction?
     async fn is_already_verified(&self, _: &Hash) -> Result<bool, E> {
         Ok(false)
+    }
+}
+
+/// A ZKPCache that always returns true, for use in tests or when we are sure that the proofs are already verified
+pub struct TrustedZKPCache;
+
+#[async_trait]
+impl<E> ZKPCache<E> for TrustedZKPCache {
+    async fn is_already_verified(&self, _: &Hash) -> Result<bool, E> {
+        Ok(true)
     }
 }
